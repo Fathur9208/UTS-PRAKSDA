df8b0bfde85d34ea2089d2e17e23869e66983dd5
khalish16 <khalish.aufa16@gmail.com>
2025-03-22 19:12:40 +0700

    Update parsing_expression.c

--- parsing_expression.c
+++ parsing_expression.c
@@ -1,1 +1,181 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+// Definisi struktur data untuk stack karakter
+typedef struct CharStackNode {
+    char data;
+    struct CharStackNode *next;
+} CharStackNode;
+
+typedef struct {
+    CharStackNode *top;
+} CharStack;
+
+CharStack* createCharStack() {
+    CharStack *stack = (CharStack*)malloc(sizeof(CharStack));
+    stack->top = NULL;
+    return stack;
+}
+
+void pushChar(CharStack *stack, char data) {
+    CharStackNode *newNode = (CharStackNode*)malloc(sizeof(CharStackNode));
+    newNode->data = data;
+    newNode->next = stack->top;
+    stack->top = newNode;
+}
+
+char popChar(CharStack *stack) {
+    if (stack->top == NULL) return '\0';
+    CharStackNode *temp = stack->top;
+    char data = temp->data;
+    stack->top = stack->top->next;
+    free(temp);
+    return data;
+}
+
+int isEmptyChar(CharStack *stack) {
+    return stack->top == NULL;
+}
+
+char topChar(CharStack *stack) {
+    if (isEmptyChar(stack)) return '\0';
+    return stack->top->data;
+}
+
+// Definisi struktur data untuk stack string
+typedef struct StringStackNode {
+    char *data;
+    struct StringStackNode *next;
+} StringStackNode;
+
+typedef struct {
+    StringStackNode *top;
+} StringStack;
+
+StringStack* createStringStack() {
+    StringStack *stack = (StringStack*)malloc(sizeof(StringStack));
+    stack->top = NULL;
+    return stack;
+}
+
+void pushString(StringStack *stack, const char *data) {
+    StringStackNode *newNode = (StringStackNode*)malloc(sizeof(StringStackNode));
+    newNode->data = strdup(data);
+    newNode->next = stack->top;
+    stack->top = newNode;
+}
+
+char* popString(StringStack *stack) {
+    if (stack->top == NULL) return NULL;
+    StringStackNode *temp = stack->top;
+    char *data = temp->data;
+    stack->top = stack->top->next;
+    free(temp);
+    return data;
+}
+
+int isEmptyString(StringStack *stack) {
+    return stack->top == NULL;
+}
+
+void freeStringStack(StringStack *stack) {
+    while (!isEmptyString(stack)) {
+        char *data = popString(stack);
+        free(data);
+    }
+    free(stack);
+}
+
+// Fungsi bantu
+int isOperator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+int getPrecedence(char op) {
+    switch(op) {
+        case '+':
+        case '-': return 1;
+        case '*':
+        case '/': return 2;
+        default: return 0;
+    }
+}
+
+char* reverseString(const char *str) {
+    int len = strlen(str);
+    char *reversed = (char*)malloc(len + 1);
+    for (int i = 0; i < len; i++) {
+        reversed[i] = str[len - i - 1];
+    }
+    reversed[len] = '\0';
+    return reversed;
+}
+
+void swapParentheses(char *str) {
+    for (int i = 0; str[i]; i++) {
+        if (str[i] == '(') str[i] = ')';
+        else if (str[i] == ')') str[i] = '(';
+    }
+}
+
+// Fungsi konversi
+char* infixToPostfix(const char *infix) {
+    CharStack *stack = createCharStack();
+    int len = strlen(infix);
+    char *postfix = (char*)malloc(2 * len + 1);
+    int j = 0;
+
+    for (int i = 0; i < len; i++) {
+        char c = infix[i];
+        if (c == ' ') continue;
+        if (isalnum(c)) {
+            postfix[j++] = c;
+        } else if (c == '(') {
+            pushChar(stack, c);
+        } else if (c == ')') {
+            while (!isEmptyChar(stack) && topChar(stack) != '(') {
+                postfix[j++] = popChar(stack);
+            }
+            popChar(stack);
+        } else if (isOperator(c)) {
+            while (!isEmptyChar(stack) && getPrecedence(topChar(stack)) >= getPrecedence(c)) {
+                postfix[j++] = popChar(stack);
+            }
+            pushChar(stack, c);
+        }
+    }
+    while (!isEmptyChar(stack)) {
+        postfix[j++] = popChar(stack);
+    }
+    postfix[j] = '\0';
+    free(stack);
+    return postfix;
+}
+
+char* postfixToInfix(const char *postfix) {
+    StringStack *stack = createStringStack();
+    int len = strlen(postfix);
+
+    for (int i = 0; i < len; i++) {
+        char c = postfix[i];
+        if (c == ' ') continue;
+        if (isalnum(c)) {
+            char operand[2] = {c, '\0'};
+            pushString(stack, operand);
+        } else if (isOperator(c)) {
+            char *op2 = popString(stack);
+            char *op1 = popString(stack);
+            char *newExpr = (char*)malloc(strlen(op1) + strlen(op2) + 4);
+            sprintf(newExpr, "(%s%c%s)", op1, c, op2);
+            pushString(stack, newExpr);
+            free(op1);
+            free(op2);
+            free(newExpr);
+        }
+    }
+    char *result = popString(stack);
+    freeStringStack(stack);
+    return result;
+}
